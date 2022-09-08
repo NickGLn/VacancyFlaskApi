69e59f06c430bc675e29fbf8b32f77aa9233569f
NickGLn <nikgutorof@gmail.com>
2022-09-09 00:33:49 +0300

    Added database

--- main.py
+++ main.py
@@ -1,40 +1,58 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 
-specializations = {"dwh": "Kimball", "cloud": "Shulz"}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-vacancies = {1: {"name": "Data Engineer", "company": "Amazon", "salary": 180000},
-             2: {"name": "Data Scientist", "company": "Microsoft", "salary": 200000},
-             3: {"name": "Data Analyst", "company": "Apple", "salary": 160000}}
+
+class VacancyModel(db.Model):
+    id =  db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    company = db.Column(db.String(100), nullable=True)
+    salary = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return f"vacancy \"{name}\" from company {company}, with salary = {str(salary)}"
+
 
 vac_args = reqparse.RequestParser()
 
 vac_args.add_argument("name", type=str, required=True )
 vac_args.add_argument("company", type=str, required=True )
-vac_args.add_argument("salary", type=str, required=False )
+vac_args.add_argument("salary", type=int, required=False )
 
 
-def abort_if_not_exists(vacancy_name):
-    if not vacancy_name:
+def abort_if_not_exists(vacancy_id):
+    if not vacancy_id:
         abort('vacancy does not exist')
 
 
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'company': fields.String,
+    'salary': fields.Integer
+}
+
+
 class Vacancy(Resource):
+
+    @marshal_with(resource_fields)
     def get(self, vacancy_id):
-        return vacancies[vacancy_id]
+        result = VacancyModel.query.filter_by(id=int(vacancy_id)).first()
+        return result
 
-    def post(self):
-        return {"Post Status": "Success"}
-
+    @marshal_with(resource_fields)
     def put(self):
-        vacancy_id = len(vacancies)+1
         args = vac_args.parse_args()
-        vacancies[vacancy_id] = args
-        return vacancies[vacancy_id]
-
+        vacancy = VacancyModel(name=args['name'], company=args['company'], salary=args['salary'])
+        db.session.add(vacancy)
+        db.session.commit()
+        return vacancy, 201
 
 api.add_resource(Vacancy, "/vacancy/<int:vacancy_id>", '/vacancy')
 
